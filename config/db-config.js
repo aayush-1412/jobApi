54113e2cd7c4957ca3160257089824a9b1ed2956
Aayush <gaayushg11@gmail.com>
2023-02-10 15:46:33 +0530

    Configured database

--- config/db-config.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-const dbconnection=()=>
-mongoose.connect("mongodb://localhost:27017/jobApi")
-.then(()=>console.log("Db connected"))
-.catch((error)=>console.log(error))
-
-export default dbconnection
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authenticateUser from "./middleware/auth.js";
 import helmet from "helmet";
 import xss from "xss-clean"
 import rateLimiter from "express-rate-limit"
+import dotenv from "dotenv"
 const app=express()
 app.set('trust proxy',1)
 app.use(rateLimiter({
@@ -18,6 +19,7 @@ app.use(express.json())
 app.use(helmet())
 app.use(cors())
 app.use(xss())
+dotenv.config()
 
 
 
@@ -32,7 +34,7 @@ app.use(xss())
 
 //db connection
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://localhost:27017/jobApi")
+mongoose.connect(process.env.MONGODB_URL)
 .then(()=>console.log("Db connected"))
 .catch((error)=>console.log(error))
 
